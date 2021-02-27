b2b867c81bf28b3d82b1aa608afd79eb4ed114c6
Livinu <livinus.tuyisenge@gmail.com>
2021-02-27 23:54:14 +0100

    Securing the webhook

--- server.rb
+++ server.rb
@@ -3,9 +3,17 @@ require "sinatra/reloader" if development?
 require 'json'
 
 post '/payload' do
-  push = JSON.parse(request.body.read)
+    request.body.rewind
+    payload_body = request.body.read
+    verify_signature(payload_body)
+    push = JSON.parse(params[:payload])
   puts "I got some JSON: #{push.inspect}"
 end
+
+def verify_signature(payload_body)
+    signature = 'sha256=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha256'), ENV['SINATRA_SECRET_TOKEN'], payload_body)
+    return halt 500, "Signatures didn't match!" unless Rack::Utils.secure_compare(signature, request.env['HTTP_X_HUB_SIGNATURE_256'])
+  end
 
 get '/' do
     "Hello World"
